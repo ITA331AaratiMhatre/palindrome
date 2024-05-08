c49c15be486de23a544488d47f335708f30b46ec
Aarti Mhatre <95845112+ITA331AaratiMhatre@users.noreply.github.com>
2024-05-08 12:26:15 +0530

    Add files via upload

---
+++ palindrome.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool isPalindrome(char *str) {
+    int left = 0;
+    int right = strlen(str) - 1;
+    
+    while (left < right) {
+        if (str[left] != str[right])
+            return false;
+        left++;
+        right--;
+    }
+    return true;
+}
+
+int main() {
+    char str[100];
+    
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Removing trailing newline character
+    if (str[strlen(str) - 1] == '\n')
+        str[strlen(str) - 1] = '\0';
+
+    if (isPalindrome(str))
+        printf("%s is a palindrome.\n", str);
+    else
+        printf("%s is not a palindrome.\n", str);
+    
+    return 0;
+}
